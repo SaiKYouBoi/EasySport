21c771257d969e7a20f2143f1e123cdf18fb630c
SaiKYouBoi <iliaselggames@gmail.com>
2026-02-18 23:01:31 +0000

    feat : Add attendances migration

---
+++ database/migrations/2026_02_18_222601_create_payments_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('member_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('class_id')->nullable()->constrained()->nullOnDelete();
+            $table->enum('type',['subscription','one_time']);
+            $table->decimal('amount',12,2);
+            $table->dateTime('paid_at');
+            $table->date('subscription_month')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payments');
+    }
+};
